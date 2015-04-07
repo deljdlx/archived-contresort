d10acb77f479edc31c6deb6173a52caa89222cec
Julien Delsescaux <jdelsesc@prismamedia.com>
2015-04-07 13:24:11 +0200

    synchro

--- application/presentation/template/layout/default.php
+++ application/presentation/template/layout/default.php
@@ -12,11 +12,58 @@
 
 	<link rel="icon" href="resource/favicon.png"/>
 	<link rel="stylesheet" href="resource/css/global.css"/>
-
+<style>
+			#forkongithub a{
+				background:#000;
+				text-decoration:none;
+				font-family:arial,sans-serif;
+				text-align:center;
+				font-weight:bold;
+				padding:5px 40px;
+				font-size:2rem;
+				line-height:4rem;
+				position:relative;
+				transition:0.5s;
+			}
+			#forkongithub a:hover{
+				background:#26437C;color:#5FC6F5;
+			}
+			#forkongithub a::before,#forkongithub a::after{
+				content:"";
+				width:100%;
+				display:block;
+				position:absolute;
+				top:1px;left:0;
+				height:1px;
+				background:#fff;
+			}
+			#forkongithub a::after{
+				bottom:1px;
+				top:auto;
+			}
+			@media screen and (min-width:800px) {
+				#forkongithub{
+					position:fixed;display:block;
+					top:0;
+					right:0;
+					width:280px;
+					overflow:hidden;
+					height:200px;
+					z-index:9999;
+				}
+				#forkongithub a{
+					width:280px;position:absolute;
+					top:55px;
+					right:-60px;
+					transform:rotate(45deg);-webkit-transform:rotate(45deg);-ms-transform:rotate(45deg);-moz-transform:rotate(45deg);-o-transform:rotate(45deg);
+					/*box-shadow:2px 2px 5px rgba(0,0,0,0.3);*/
+				}
+			}
+</style>
 
 </head>
 <body>
-
+<span id="forkongithub"><a href="https://github.com/ElBiniou/contresort">Fork me on GitHub</a></span>
 
 <?=$navigationBar;?>
 
