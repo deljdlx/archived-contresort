2cf0bf5bbe886b85250d80a7fb953c34751746a1
ElBiniou <deljdlx@gmail.com>
2015-05-02 15:47:50 +0100

    test unitaire web

--- test/web/bootstrap.php
+++ test/web/bootstrap.php
@@ -2,3 +2,8 @@
 
 include(__DIR__.'/../../source/contresort.php');
 
+
+
+$extension=new \Contresort\Extension('Contresort\Test', '../../extension/contresort/test');
+
+
--- test/web/test/router.test.php
+++ test/web/test/router.test.php
@@ -3,13 +3,16 @@
 class RouterTest extends \Contresort\Test\Launcher
 {
 	public function testMethod() {
+
 		echo 'Instanciate a new Application';
 		$application=new \ContreSort\Application('Demo');
 
-
-		$application->route(null, '`.*`', function() {
+		$application->get('`.*`')
+			->addAction(function() {
 			$this->output='hello';
+			return true;
 		});
+
 
 		$application->run();
 		if($application->getOutput()=='hello') {
